cf7fd3c049dc2d5c43b4e529a6f1b1027ea88798
Ayush Jaju <ayushjjaju@gmail.com>
2025-10-15 10:23:19 +0530

    First Commit

---
+++ CalcUI.js
@@ -0,0 +1,5 @@
+import { AddNumbers, MultiplyNumbers, SubtractNumbers } from "./Calculator.js"
+
+console.log(AddNumbers(3,2));
+console.log(SubtractNumbers(3,2));
+console.log(MultiplyNumbers(3,2));
---
+++ Calculator.js
@@ -0,0 +1,14 @@
+export function AddNumbers(a,b)
+{
+    return a+b;
+}
+
+export function SubtractNumbers(a,b)
+{
+    return a-b;
+}
+
+export function MultiplyNumbers(a,b)
+{
+    return a*b;
+}
